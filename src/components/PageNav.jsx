312c7768926f1e55289ff3f2e79df5e7ae4a1075
bolaji87 <sakabolaji77@gmail.com>
2024-08-21 10:48:52 +0100

    feat: create pageNav

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+
+function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/">home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">product</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
