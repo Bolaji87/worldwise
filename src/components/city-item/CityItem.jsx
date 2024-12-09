e15eb2b9f01d7a2655adb9d96f23a559f0ddc84b
bolaji87 <sakabolaji77@gmail.com>
2024-12-09 14:45:57 +0100

    feat : use deleteCity event handler to delete city

--- src/components/city-item/CityItem.jsx
+++ src/components/city-item/CityItem.jsx
@@ -11,9 +11,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currenCity } = useCities();
+  const { currenCity, deleteCity } = useCities();
   const { cityName, country, emoji, date, notes, id, position } = city;
 
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
   return (
     <li>
       <Link
@@ -25,7 +29,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
