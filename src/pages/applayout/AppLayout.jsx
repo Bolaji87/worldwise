ce55e72968221495424bb1031b16f485ee27ca4b
bolaji87 <sakabolaji77@gmail.com>
2024-12-19 17:47:29 +0100

    feat: use User component in AppLout component

--- src/pages/applayout/AppLayout.jsx
+++ src/pages/applayout/AppLayout.jsx
@@ -3,12 +3,14 @@ import AppNav from "../../components/appnav/AppNav";
 import styles from "./AppLayout.module.css";
 import SideBar from "../../components/side-bar/SideBar";
 import Map from "../../components/map/Map";
+import User from "../../components/user/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
       <Map />
+      <User />
     </div>
   );
 }
