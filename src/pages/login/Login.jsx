7e5269183d00a3da2caa85be522f4529245c2dbc
bolaji87 <sakabolaji77@gmail.com>
2024-12-19 17:38:12 +0100

    feat: call the login function to authenticate the login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../../components/page-nav/PageNav";
+import { useAuth } from "../../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/button/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const { login, isAuthenticated } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("form submitted");
+    if (email && password) login(email, password);
+  };
+
+  useEffect(() => {
+    console.log("isAuthenticated", isAuthenticated);
+    if (isAuthenticated) navigate("/app", { replace: true });
+  }, [isAuthenticated, navigate]);
+
   return (
     <main className={styles.login}>
       <PageNav />
+
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
@@ -32,7 +52,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button onClick={handleSubmit} type="primary">
+            Login
+          </Button>
         </div>
       </form>
     </main>
